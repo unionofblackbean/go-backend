85332ec3cc98d281cb6ed97b1519be67ff07d673
Leung Yau Ming <leungyauming2005@gmail.com>
2021-05-15 15:50:51 +0800

    service(auth): init database pool and rest api

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/unionofblackbean/backend/services/auth/rest"
+import (
+	"log"
+
+	"github.com/unionofblackbean/backend/pkg/database"
+	"github.com/unionofblackbean/backend/services/auth/rest"
+)
 
 func main() {
+	pool, err := database.Pool(
+		"backend", "backend",
+		"127.0.0.1", 5432,
+		"backend",
+	)
+	if err != nil {
+		log.Fatalf("failed to establish connection with database -> %v", err)
+	}
+
+	rest.Init(pool)
 	rest.Run("127.0.0.1", 8080)
 }
